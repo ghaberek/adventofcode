dbb7acbe57cbd68d07a5f4634ed07853b08dffb6
Gregory Haberek <ghaberek@gmail.com>
2022-12-14 19:32:22 +0000

    Add 2022/06

---
+++ 2022/06/part2.ex
@@ -0,0 +1,51 @@
+
+include std/io.e
+
+constant MESSAGE_SIZE = 14
+
+integer offset = 0, start = 0
+sequence buffer = {}
+
+integer ch
+
+while not find( ch, {EOF,'\r','\n'} ) with entry do
+
+	-- increment counter
+	offset += 1
+
+	-- convert ch to integer
+	ch = ch - 'a' + 1
+
+	-- append character to the buffer
+	buffer = insert( buffer, ch, 1 )
+
+	-- check buffer length
+	if length( buffer ) < MESSAGE_SIZE then
+		-- not enough characters
+		ch = getc( STDIN )
+		continue
+	end if
+
+	-- input characters are all 'a'-'z'
+	-- so ch value will always be 1-26
+	sequence counts = repeat( 0, 26 )
+
+	-- count each character
+	for i = 1 to MESSAGE_SIZE do
+		ch = buffer[i]
+		counts[ch] += 1
+	end for
+
+	-- here, "counts > 1" will be a sequence of
+	-- boolean results (0 or 1), so if no count
+	-- is greater than 1 then we have our start
+	if find( 1, counts > 1 ) = 0 then
+		start = offset
+		exit
+	end if
+
+entry
+	ch = getc( STDIN )
+end while
+
+? start
--- 2022/README.md
+++ 2022/README.md
@@ -7,7 +7,7 @@ My solutions for [Advent of Code 2022](https://adventofcode.com/2022) written in
 - [x] [Day 3: Rucksack Reorganization](03/)
 - [x] [Day 4: Camp Cleanup](04/)
 - [x] [Day 5: Supply Stacks](05/)
-- [ ] Day 6: Tuning Trouble
+- [x] [Day 6: Tuning Trouble](06/)
 - [ ] Day 7: No Space Left On Device
 - [ ] Day 8: Treetop Tree House
 - [ ] Day 9: Rope Bridge
@@ -15,4 +15,4 @@ My solutions for [Advent of Code 2022](https://adventofcode.com/2022) written in
 - [ ] Day 11: Monkey in the Middle
 - [ ] Day 12: Hill Climbing Algorithm
 - [ ] Day 13: Distress Signal
-
+- [ ] Day 14: Regolith Reservoir
